87faf65ab3aafcb9b52b0219d51319dbac3cf3e5
victorgtz91 <134213968+victorgtz91@users.noreply.github.com>
2023-07-13 13:55:45 -0400

    Update application.py

--- application.py
+++ application.py
@@ -24,7 +24,7 @@ img_portfolio = Image.open("images/portfolio.png")
 
 # HEADER SECTION #
 with st.container():
-st.markdown("""
+    st.markdown("""
         <style>
         .center {
             display: flex;
@@ -36,7 +36,8 @@ st.markdown("""
         </style>
         """, unsafe_allow_html=True)
 
-    st.markdown("<div class='center'>", unsafe_allow_html=True)    st.subheader("hi, I am victor")
+    st.markdown("<div class='center'>", unsafe_allow_html=True)
+    st.subheader("hi, I am victor")
     st.title("financial geek specializing in portfolio allocation and wealth management")
     st.write("I am passionate about finding ways to use next-gen tech to solve everyday problems.")
     st.write("[linkedin](https://www.linkedin.com/in/victor-gutierrez-z/)")
