5348dd16c125a73bc93bd2851a693d66fe872bfd
victorgtz91 <134213968+victorgtz91@users.noreply.github.com>
2023-07-13 13:59:46 -0400

    Update application.py

--- application.py
+++ application.py
@@ -25,23 +25,14 @@ img_portfolio = Image.open("images/portfolio.png")
 # HEADER SECTION #
 with st.container():
     st.markdown("""
-        <style>
-        .center {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            text-align: center;
-        }
-        </style>
+        <div style="display: flex; flex-direction: column; align-items: center;">
+            <h3>hi, I am victor</h3>
+            <h1>financial geek specializing in portfolio allocation and wealth management</h1>
+            <p>I am passionate about finding ways to use next-gen tech to solve everyday problems.</p>
+            <p><a href="https://www.linkedin.com/in/victor-gutierrez-z/">linkedin</a></p>
+        </div>
         """, unsafe_allow_html=True)
 
-    st.markdown("<div class='center'>", unsafe_allow_html=True)
-    st.subheader("hi, I am victor")
-    st.title("financial geek specializing in portfolio allocation and wealth management")
-    st.write("I am passionate about finding ways to use next-gen tech to solve everyday problems.")
-    st.write("[linkedin](https://www.linkedin.com/in/victor-gutierrez-z/)")
-    st.markdown("</div>", unsafe_allow_html=True)
 
 
 # WHAT I DO #
